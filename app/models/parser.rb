9c49e3cb2223276e572f2b765d9bf6484a28b6a4
Elias J. Perez <eperez@pinch.me>
2015-11-25 10:57:09 -0500

    Check for nil on header

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -24,7 +24,7 @@ class Parser
   def self.header(system, spreadsheet)
     header = []
     (6..12).each do |i|
-      header << spreadsheet.row(i)[1] unless spreadsheet.row(i)[1].downcase.eql?("all")
+      header << spreadsheet.row(i)[1] unless spreadsheet.row(i)[1].blank? && spreadsheet.row(i)[1].downcase.eql?("all")
     end
     header
   end
